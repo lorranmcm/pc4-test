35507c853ef7dcfbf8f8388e12be75369dcae03e
Lorran Monteiro <lorranmcm@gmail.com>
2021-04-27 15:32:05 -0300

    Crud e Reatividade funcionando

--- js/app.js
+++ js/app.js
@@ -1,52 +1,80 @@
 let app = new Vue({
   el: "#app",
   data: {
-    name: 'Employee App',
-    employee: {
-      "name": '',
-      "email":'',
-      "department":'',
-      "occupation":'',
-      "profile_image":'',
-      "cpf":'',
-      "role":'',
-      "active":''
-    }
+      employees: [],
+      name: 'Employee App',
+      employee: {
+        name: '',
+        email:'',
+        department:'',
+        occupation:'',
+        profile_image:'',
+        cpf:'',
+        role:'',
+        active:''
+      },
+      isEdit: false
   },
   created() {
-    this.employees = JSON.parse(localStorage.getItem('employeesApp'));
+      this.employees = JSON.parse(localStorage.getItem('employeesApp'));
   },
   methods: {
-    saveemployee(employee) {
-      let employees = localStorage.getItem('employeesApp');
+      saveEmployee(employee) {
 
-      employee.id = new Date().getTime();
+          let employees = localStorage.getItem('employeesApp');
 
-      if(employees) {
-        employees = JSON.parse(employees);
-        employees.push(employee);
-      } else {
-        employees = [employee];
+          employee.id = new Date().getTime();
+
+          if(employees) {
+              
+              employees = JSON.parse(employees);
+              employees.push(employee);
+
+          } else {
+              employees = [employee];
+          }
+
+          this.employees = employees;
+
+          localStorage.setItem('employeesApp', JSON.stringify(employees));
+
+          location.reload();
+      },
+      removeEmployee(employeeId) {
+          let employees = localStorage.getItem('employeesApp');
+
+          if(!employees) return;
+
+          employees = JSON.parse(employees);
+
+          employees = employees.filter((employee) => {
+              return employee.id != employeeId;
+          });
+
+          this.employees = employees;
+
+          localStorage.setItem('employeesApp', JSON.stringify(employees));
+      },
+      editEmployee(employee) {
+          this.employee = employee;
+          this.isEdit = true;
+      },
+      updateEmployee(employee) {
+          let employees = this.employees.map(employeeMap => {
+              if(employeeMap.id == employee.id) {
+                  return employee;
+              }
+
+              return employeeMap;
+          });
+
+          this.employees = employees;
+          this.isEdit = false;
+
+          localStorage.setItem('employeesApp', JSON.stringify(employees));
+
+          location.reload();
+
       }
-      
-      this.employees = employees;
-
-      localStorage.setItem('employeesApp', JSON.stringify(employees))
-    },
-    removeemployee(employeeId) {
-      let employees = localStorage.getItem('employeesApp');
-
-      if(!employees) return;
-
-      employees = JSON.parse(employees);
-
-      employees = employees.filter((employee) => {
-        return employee.id != employeeId;
-      });
-
-      this.employees = employees;
-
-      localStorage.setItem('employeesApp', JSON.stringify(employees));
-    }
   }
 });
